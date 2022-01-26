417aa695529a93ed2af698ff53dab372b4897c07
Douglas Duarte <douglasduarte.eng@gmail.com>
2022-01-26 20:06:17 -0300

    Custom hook

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Form.module.css';
 
 const Form = () => {
+    const [location, setLocation] = useState('');
+    const locationChangeHandler = e => {
+        setLocation(e.target.value);
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if (!location || location === '') return;
+    };
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <input
                 aria-label="location"
                 type="text"
                 className={`${styles.input} form-control`}
                 placeholder="Search for location"
                 required
+                value={location}
+                onChange={locationChangeHandler}
             />
 
             <button type="submit" className={styles.button}>
--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,11 +1,29 @@
 import React, { Fragment } from 'react';
 
 import Header from '../Header';
+import Form from '../Form';
+import Error from '../Error';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
+import useForecast from '../../hooks/useForecast';
+
+import styles from './Page.module.css';
 
 const Page = () => {
+    const { isError, isLoading, forecast } = useForecast();
     return (
         <Fragment>
             <Header />
+            <div className={styles.box}>
+                {/* Form */}
+                <Form />
+                {/* Error */}
+                {isError && <Error message={isError} />}
+                {/* Loader */}
+                {isLoading && <Loader />}
+            </div>
+            {/* Forecast */}
+            {forecast && <Forecast />}
         </Fragment>
     );
 };
